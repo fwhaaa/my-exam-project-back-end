f40ddb21db6e821a2424ceddc1f8414b5139f83c
fwhaaa <2071743855@qq.com>
2024-01-23 21:45:20 +0800

    feat: 新增试卷列表

--- controller/paper.js
+++ controller/paper.js
@@ -24,3 +24,77 @@ exports.addPaper= async function(ctx, next){
 		};
   }
 };
+
+exports.paperList = async function(ctx, next){
+	try {
+    const data = ctx.request.body;
+        console.log("----data",data)
+		const connection = connectionModel.getConnection();
+		const query = bluebird.promisify(connection.query.bind(connection));
+		const sql_query = `SELECT * FROM paper;`
+		console.log('sqlquery',sql_query);
+		const results = await query(sql_query);
+		ctx.body = {
+			respCode: 1,
+			results
+		};
+		console.log("----results",results)
+		connection.end();
+  }catch(e){
+    console.log('paperList error', e.message, e.stack);
+    ctx.body = {
+			respCode: e.code || -1,
+			respMsg: e.message
+		};
+  }
+};
+
+
+exports.deletePaper = async function(ctx, next){
+	try {
+    const data = ctx.request.body;
+        console.log("----data",data)
+		const connection = connectionModel.getConnection();
+		const query = bluebird.promisify(connection.query.bind(connection));
+		const sql_query = `DELETE  FROM paper WHERE id = '${data.id}';`
+		console.log('sqlquery',sql_query);
+		const results = await query(sql_query);
+		ctx.body = {
+			respCode: 1,
+			results
+		};
+		console.log("----results",results)
+		connection.end();
+  }catch(e){
+    console.log('deletePaper error', e.message, e.stack);
+    ctx.body = {
+			respCode: e.code || -1,
+			respMsg: e.message
+		};
+  }
+};
+
+
+exports.editPaper = async function(ctx, next){
+	try {
+    const data = ctx.request.body;
+        console.log("----data",data)
+		const connection = connectionModel.getConnection();
+		const query = bluebird.promisify(connection.query.bind(connection));
+		const sql_query = `UPDATE paper SET subject = '${data.subject}',papername = '${data.papername}',singlescore = '${data.singlescore}',multiplescore = '${data.multiplescore}',judgescore = '${data.judgescore}',saqscore = '${data.saqscore}' WHERE id = '${data.id}'; `
+		console.log('sqlquery',sql_query);
+		const results = await query(sql_query);
+		ctx.body = {
+			respCode: 1,
+			results
+		};
+		console.log("----results",results)
+		connection.end();
+  }catch(e){
+    console.log('editPaper error', e.message, e.stack);
+    ctx.body = {
+			respCode: e.code || -1,
+			respMsg: e.message
+		};
+  }
+};
--- routes/paper.js
+++ routes/paper.js
@@ -7,7 +7,9 @@ var rourter = new Router({
 const paper = require('../controller/paper');
 
 rourter.post('/paperManagement/add',paper.addPaper);
-
+rourter.get('/paperManagement/list',paper.paperList);
+rourter.post('/paperManagement/delete',paper.deletePaper)
+rourter.post('/paperManagement/edit',paper.editPaper);
 
 
 
