feb1144c386d239052abfb10956fecf11268b35a
fwhaaa <2071743855@qq.com>
2024-02-29 21:08:22 +0800

    feat：新增学生端登录

--- controller/login.js
+++ controller/login.js
@@ -50,3 +50,49 @@ exports.userLogin = async function(ctx, next){
 		};
   }
 };
+
+exports.studentLogin = async function(ctx, next){
+	try {
+        const data = ctx.request.body;  
+        console.log("----data",data)
+		const connection = connectionModel.getConnection();
+		const query = bluebird.promisify(connection.query.bind(connection));
+		const urlquery = ctx.request.query;
+		console.log('!!urlquery',urlquery);
+		// const sql_query = urlquery.id ? `SELECT * FROM paper where id = '${urlquery.id}'; ` : `SELECT * FROM paper;`
+    
+        const sql_query =  `SELECT * FROM student WHERE studentname = '${data.username}'; ` 
+		console.log('sqlquery',sql_query);
+		const results = await query(sql_query);
+        if(results.length>0){
+           if(results[0].password === data.password){
+            console.log('----enter1');
+        	ctx.body = {
+                respCode: 1,
+                results
+            };
+           } else{
+            console.log('----enter2');
+            ctx.body = {
+                respCode:  -1,
+                respMsg: "密码错误"
+            };
+           }
+        } else {
+            console.log('----enter3');
+            ctx.body = {
+                respCode:  -1,
+                respMsg: "用户名错误"
+            };
+        }
+		console.log("----results",results)
+		connection.end();
+  }catch(e){
+    console.log('----enter4');
+    console.log('userLogin error', e.message, e.stack);
+    ctx.body = {
+			respCode: e.code || -1,
+			respMsg: e.message
+		};
+  }
+};
--- routes/login.js
+++ routes/login.js
@@ -9,5 +9,7 @@ const login = require('../controller/login');
 
 router.post('/login', login.userLogin);
 
+router.post('/student', login.studentLogin);
+
 
 module.exports = router;
